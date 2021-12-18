c397581461cc26769ce55b5bb702857c873f35ce
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-18 10:24:11 -0800

    add push, pop, and get-stack method to stack class

--- stack.py
+++ stack.py
@@ -1,3 +1,15 @@
 class Stack:
     def __init__(self):
         self.items = []
+        pass
+
+    def push(self, item):
+        self.items.append(item)
+        pass
+
+    def pop(self):
+        return self.items.pop()
+
+    def get_stack(self):
+        return self.items
+    
