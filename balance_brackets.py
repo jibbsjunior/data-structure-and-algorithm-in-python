b4d1ea6536d16f3fac7b214b4085fe030cdba633
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-18 11:42:04 -0800

    implement the full function that check if brackets is balance or not

--- balance_brackets.py
+++ balance_brackets.py
@@ -9,3 +9,40 @@ def is_match(p1, p2):
         return True
     else:
         return False
+
+def is_parent_balanced(parent_string):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(parent_string) and is_balanced:
+        parent = parent_string[index]
+        if parent in "([{":
+            s.push(parent)
+        else:
+            if s.is_empty():
+                is_balanced = False
+                break
+            else:
+                top = s.pop()
+                if not is_match(top, parent):
+                    is_balanced = False
+                    break
+                pass
+            pass
+        index += 1
+        pass
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
+
+
+print("String : (((({})))) Balanced or not?")
+print(is_parent_balanced("(((({}))))"))
+
+print("String : [][]]] Balanced or not?")
+print(is_parent_balanced("[][]]]"))
+
+print("String : [][] Balanced or not?")
+print(is_parent_balanced("[][]"))
