88cac23be489c4ecc73ffb882be5a63512671eec
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-18 10:30:10 -0800

    add is_empty method to stack family

--- stack.py
+++ stack.py
@@ -13,12 +13,16 @@ class Stack:
     def get_stack(self):
         return self.items
 
+    def is_empty(self):
+        return self.items == []
+
 stack = Stack()
-stack.push("Ajibola")
-stack.push("Adebayo")
-print(stack.get_stack())
-stack.push("Rilwan")
-print(stack.get_stack())
-stack.pop()
-print(stack.get_stack())
+# stack.push("Ajibola")
+# stack.push("Adebayo")
+# print(stack.get_stack())
+# stack.push("Rilwan")
+# print(stack.get_stack())
+# stack.pop()
+# print(stack.get_stack())
+print(stack.is_empty())
     
