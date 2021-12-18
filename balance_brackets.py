19dcf270bd026a0a5e82ee5b5295fdabc7b84cac
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-18 11:28:28 -0800

    implementing balance brackets function

---
+++ balance_brackets.py
@@ -0,0 +1,11 @@
+from stack import Stack
+
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    else:
+        return False
