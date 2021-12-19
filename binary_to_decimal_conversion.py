3a683b84faae09ecf28be3f8abe9012c2c46d5cc
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-18 21:04:59 -0800

    implementing decimal to binary conversion function

---
+++ binary_to_decimal_conversion.py
@@ -0,0 +1,4 @@
+# In this coding exercise, you are required to use the stack data structure to convert integer values to their binary equivalent.
+# I will use the division by 2 method in this challenge to convert a decimal number to binary
+# print(60 % 2)
+
