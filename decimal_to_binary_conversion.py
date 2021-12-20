dbb3f016b6f08665afba65e73ac9b0b2828cbf5e
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-20 09:19:02 -0800

    optimize the solution to use just 1 argument

--- decimal_to_binary_conversion.py
+++ decimal_to_binary_conversion.py
@@ -17,8 +17,31 @@ def dec_to_bin(stack, dec_num):
         bin_num += str(stack.pop())
     return bin_num
 
+# Another method that optimize the function to use 1 argument
+def decimal_to_binary(dec_num):
+    if dec_num == 0:
+        return 0
 
-s = Stack()
-dec = 27
-print(dec_to_bin(s, dec))
+    s = Stack()
+
+    while dec_num > 0:
+        remainder = dec_num % 2
+        s.push(remainder)
+        dec_num = dec_num // 2
+
+    bin_num = ""
+    while not s.is_empty():
+        bin_num += str(s.pop())
+
+    return bin_num
+
+# s = Stack()
+# # dec = 112
+# print(dec_to_bin(s, 12))
+# print(dec_to_bin(s, 112))
+# print(dec_to_bin(s, 60))
+# print(dec_to_bin(s, 27))
+# print(dec_to_bin(s, 48))
+# print(dec_to_bin(s, 98))
+print(decimal_to_binary(21))
 
