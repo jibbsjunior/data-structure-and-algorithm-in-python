698eb8beabde69408890c281bc03574102984a46
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-20 10:37:44 -0800

    implementing linked list in python

---
+++ singly_list_list.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+        pass
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        pass
