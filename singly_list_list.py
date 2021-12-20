61d0745ab9c106f83f9a96a754c63adcf691415f
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-20 12:59:39 -0800

    added print_list method to LinkedList class

--- singly_list_list.py
+++ singly_list_list.py
@@ -14,4 +14,22 @@ class LinkedList:
         if self.head is None:
             self.head = new_node
             return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+node = LinkedList()
+node.append("A")
+node.append("B")
+node.append("C")
+node.append("D")
+node.append("E")
+print(node.print_list())
     
