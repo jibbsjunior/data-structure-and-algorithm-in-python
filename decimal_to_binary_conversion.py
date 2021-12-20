fedf683003d36f3e9f3b76f3f9ee4905a43f645b
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-20 09:00:39 -0800

    solution now produce accurate results based on different inputs

--- decimal_to_binary_conversion.py
+++ decimal_to_binary_conversion.py
@@ -5,30 +5,18 @@ from stack import Stack
 def dec_to_bin(stack, dec_num):
     value = []
     bin_num = []
-    # stack.push(dec_num // 2)
-    # temp = dec_num//2
     value.append(dec_num)
     while dec_num > 1:
         value.append(dec_num//2)
-        # stack.push(value[i]%2)
-        # dec_to_bin//2
         dec_num = dec_num//2
-        # while not stack.is_empty():
-        #     bin_num += stack.pop()                                                        
-    # return value
     for i in value:
         stack.push(i%2)
-        # print(i)
-        # bin_num.append(i)
     while not stack.is_empty():
         bin_num.append(stack.pop())
-    # print(value)
     return bin_num
 
 
 s = Stack()
-dec = 60
+dec = 27
 print(dec_to_bin(s, dec))
-# print(dec_to_bin(60))
-
 
