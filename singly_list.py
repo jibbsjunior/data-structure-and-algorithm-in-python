ed57cce10865c97ed39cba1010aa02968d9452ad
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-20 18:44:13 +0100

    implement count-occurrences method(iterative method)

--- singly_list.py
+++ singly_list.py
@@ -242,6 +242,16 @@ class LinkedList:
         else:
             print("Please select either method 1 or 2")
 
+    def count_occurences_iterative(self, data):
+        count = 0
+        cur = self.head
+
+        while cur:
+            if cur.data == data:
+                count += 1
+            cur = cur.next
+        return count
+
 
 
 
